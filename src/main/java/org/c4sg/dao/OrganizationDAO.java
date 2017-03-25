bbf7b7c1181a40d783d77c8c42b7e910a88a3377
Felipe A L Pereira <felipealves.gnu@gmail.com>
2017-03-25 15:36:58 -0300

    Refactor - closes #15

--- src/main/java/org/c4sg/dao/OrganizationDAO.java
+++ src/main/java/org/c4sg/dao/OrganizationDAO.java
@@ -1,16 +1,28 @@
 package org.c4sg.dao;
 
+import org.c4sg.entity.Organization;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
-import org.c4sg.entity.Organization;
-import org.springframework.data.repository.CrudRepository;
-
 public interface OrganizationDAO extends CrudRepository<Organization, Integer> {
-	
-	Organization findByName(String name);
-	List<Organization> findAll();
-	Organization findOne(Integer id);
-	/*@Query("SELECT o FROM Organization o WHERE LOWER(o.name) LIKE LOWER(CONCAT('%',:keyword,'%')) OR LOWER(o.description) LIKE LOWER(CONCAT('%',:keyword,'%'))")
-	List<Organization> findByKeyword(String keyword);*/
-	List<Organization> findByNameLikeOrDescriptionLikeAllIgnoreCase(String name, String description);
+    String FIND_BY_NAME_OR_DESCRIPTION = "SELECT o FROM Organization o " +
+                                            "WHERE o.name LIKE CONCAT('%', :name, '%') " +
+                                                "OR o.description LIKE CONCAT('%', :description, '%')";
+
+    Organization findByName(String name);
+
+    List<Organization> findAll();
+
+    Organization findOne(Integer id);
+
+    /*@Query("SELECT o FROM Organization o WHERE LOWER(o.name) LIKE LOWER(CONCAT('%',:keyword,'%')) OR LOWER(o.description) LIKE LOWER(CONCAT('%',:keyword,'%'))")
+    List<Organization> findByKeyword(String keyword);*/
+
+    @Query(FIND_BY_NAME_OR_DESCRIPTION)
+    List<Organization> findByNameOrDescription(@Param("name") String name, @Param("description") String description);
+
+//	List<Organization> findByNameLikeOrDescriptionLikeAllIgnoreCase(String name, String description);
 }
