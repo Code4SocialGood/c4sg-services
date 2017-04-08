738d9180d5886aca2def37a2adc6b33702dfe006
Uche B <barryajokubi@yahoo.com>
2017-04-07 21:50:53 -0500

    Setup Java Logging Framework #17
    Add logback files for dev and prod.
    logback uses sl4j library.

--- src/main/java/org/c4sg/C4SgApplication.java
+++ src/main/java/org/c4sg/C4SgApplication.java
@@ -2,12 +2,14 @@ package org.c4sg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
 
 @EnableAsync
 @SpringBootApplication
 public class C4SgApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(C4SgApplication.class, args);
 	}
 }
