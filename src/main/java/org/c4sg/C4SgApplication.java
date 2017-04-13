21f84343d2eade73cd6c0dccedcd797f1b44dad7
Uche B <barryajokubi@yahoo.com>
2017-04-13 08:08:36 -0500

    Automate logback file selection using appropriate application.properties

--- src/main/java/org/c4sg/C4SgApplication.java
+++ src/main/java/org/c4sg/C4SgApplication.java
@@ -1,9 +1,11 @@
 package org.c4sg;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
 
 
 @EnableAsync
