69dce5959468adba6c48155a0aad2d644c0f663b
jxwang16 <jxwang16@gmail.com>
2017-04-15 16:22:32 -0500

    fix based on comments

--- src/main/java/org/c4sg/dao/OrganizationDAO.java
+++ src/main/java/org/c4sg/dao/OrganizationDAO.java
@@ -1,11 +1,12 @@
 package org.c4sg.dao;
 
+import java.util.List;
+
 import org.c4sg.entity.Organization;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface OrganizationDAO extends CrudRepository<Organization, Integer> {
     String FIND_BY_NAME_OR_DESCRIPTION = "SELECT o FROM Organization o " +
--- src/main/java/org/c4sg/service/impl/ProjectServiceImpl.java
+++ src/main/java/org/c4sg/service/impl/ProjectServiceImpl.java
@@ -6,6 +6,8 @@ import static org.c4sg.constant.Directory.PROJECT_UPLOAD;
 import static org.c4sg.constant.Format.IMAGE;
 
 import java.io.File;
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 
 import org.c4sg.constant.UserProjectStatus;
@@ -14,6 +16,7 @@ import org.c4sg.dao.UserDAO;
 import org.c4sg.dao.UserProjectDAO;
 import org.c4sg.dto.CreateProjectDTO;
 import org.c4sg.dto.ProjectDTO;
+import org.c4sg.entity.Organization;
 import org.c4sg.entity.Project;
 import org.c4sg.entity.User;
 import org.c4sg.entity.UserProject;
@@ -145,6 +148,13 @@ public class ProjectServiceImpl implements ProjectService {
         } else {
             localProject = projectDAO.save(
             		projectMapper.getProjectEntityFromCreateProjectDto(createProjectDTO));
+
+            // Updates projectUpdateTime for the organization
+            Organization localOrgan = localProject.getOrganization(); 
+            localOrgan.setProjectUpdatedTime(new Date(Calendar.getInstance().getTime().getTime())); 
+            organizationDAO.save(localOrgan);
+            //Date currentTime = new Date(Calendar.getInstance().getTime().getTime());
+            //Integer organizationId = organizationDAO.updateProjectUpdatedTime(currentTime, createProjectDTO.getOrganizationId());
         }
 
         return localProject;
