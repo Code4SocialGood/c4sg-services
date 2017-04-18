0632cc518d5aaca60432c702f1eb9370a49661c5
karthijey <karthijey@yahoo.com>
2017-04-18 03:29:36 -0400

    new endpoints added to skill controller

--- src/main/java/org/c4sg/service/SkillService.java
+++ src/main/java/org/c4sg/service/SkillService.java
@@ -3,8 +3,12 @@ package org.c4sg.service;
 import java.util.List;
 
 import org.c4sg.dto.SkillDTO;
+import org.c4sg.dto.SkillUserCountDTO;
 
 public interface SkillService {
 
     List<SkillDTO> findSkills();
+    List<SkillUserCountDTO> findSkillsWithUserCount();
+    List<String> findSkillsForUser(Integer id);
+    List<String> findSkillsForProject(Integer id);
 }
