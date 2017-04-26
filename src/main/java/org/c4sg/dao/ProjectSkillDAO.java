22455b66ba23e6a03f52f95d491a52646632e608
karthijey <karthijey@yahoo.com>
2017-04-26 02:29:53 -0400

    indentation fix

--- src/main/java/org/c4sg/dao/ProjectSkillDAO.java
+++ src/main/java/org/c4sg/dao/ProjectSkillDAO.java
@@ -10,17 +10,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface ProjectSkillDAO extends CrudRepository<ProjectSkill, Long> {
-	ProjectSkill findByProject_IdAndSkill_Id(Integer projectId, Integer skillId);
+    ProjectSkill findByProject_IdAndSkill_Id(Integer projectId, Integer skillId);
     List<ProjectSkill> findByProjectId(Integer ProjectId);
     int countByProjectIdAndSkillId(Integer id, Integer skillId);
-	int countByProjectId(Integer id);
+    int countByProjectId(Integer id);
 	
-	@Transactional
-	void deleteByProjectId(Integer id);
+    @Transactional
+    void deleteByProjectId(Integer id);
     
     String FIND_SKILL_FOR_PROJECT ="select s.skillName as skillName "
-				      			  +"from ProjectSkill ps inner join ps.skill s where ps.project.id= :id order by ps.displayOrder";
+				  +"from ProjectSkill ps inner join ps.skill s where ps.project.id= :id order by ps.displayOrder";
 
-	@Query(FIND_SKILL_FOR_PROJECT)
-	List<Map<String, Object>> findSkillsByProjectId(@Param("id") Integer id);	
+    @Query(FIND_SKILL_FOR_PROJECT)
+    List<Map<String, Object>> findSkillsByProjectId(@Param("id") Integer id);	
 }
