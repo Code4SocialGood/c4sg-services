b6e8e3c0dcf0e019e0b318878b25aaf1a1b3febc
Soham Ashodiya <sohamashodiya@gmail.com>
2017-07-20 07:50:10 -0700

    Refactored Code into Smaller Methods

--- src/main/java/org/c4sg/controller/AllForGoodFeedController.java
+++ src/main/java/org/c4sg/controller/AllForGoodFeedController.java
@@ -31,57 +31,78 @@ public class AllForGoodFeedController {
 	@Autowired
     private OrganizationService organizationService;
 	
+	private List<OrganizationDTO> organizations = organizationService.findOrganizations();
+
+    private FeedDTO afg = new FeedDTO();
+    
 	@CrossOrigin
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
     @ApiOperation(value = "Find all projects", notes = "Returns a collection of projects")
 	public FeedDTO getProjects() {
-		List<OrganizationDTO> organizations = organizationService.findOrganizations();
-	    FeedDTO afg = new FeedDTO();
 	    afg.setOrganizations(organizations);
+	    addFeedInfo();
+		List<ProjectDTO> projects = new ArrayList<ProjectDTO>(); 
+		for (OrganizationDTO o : organizations) {
+			o.setLocation(populateFeedLocation(o));
+			for (int i = 0; i < o.getProjects().size(); i++) {
+				o.getProjects().get(i).setDateTimeDurations(populateDateTimeDurations());
+				List<FeedSponsoringOrganizationDTO> sponsoringOrganizations = new ArrayList<FeedSponsoringOrganizationDTO>();
+				sponsoringOrganizations.add(populateSponsoringOrganization(o, i));
+				o.getProjects().get(i).setSponsoringIds(sponsoringOrganizations);
+				populateRemoteFlag(o, i);
+				projects.add(o.getProjects().get(i));
+			} 
+			afg.setProjects(projects);
+		}
+        return afg;
+    }
+	
+	private void addFeedInfo() {
 	    FeedInfoDTO feedInfo = new FeedInfoDTO();
 	    Date currentDateTime = new Date();
 	    SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss");
 	    String tempDate = ft.format(currentDateTime);
 	    feedInfo.setCreatedDateTime(tempDate);
 	    afg.setFeedInfo(feedInfo);
-		List<ProjectDTO> projects = new ArrayList<ProjectDTO>(); 
-	    //TODO: Maybe move this to another place?
-		for (OrganizationDTO o : organizations) {
-			FeedLocationDTO location = new FeedLocationDTO();
-			location.setCity(o.getCity());
-			location.setPostalCode(o.getZip());
-			if (o.getAddress1() == null || o.getAddress2() == null) {
-				if(o.getAddress1() == null && !((o.getAddress1() == null) && (o.getAddress2() == null))){
-					location.setStreetAddress1(o.getAddress2());
-				} else if (o.getAddress2() == null && !(o.getAddress1() == null && o.getAddress2() == null)) {
-					location.setStreetAddress1(o.getAddress1());
-				} 
-			} else {
-				location.setStreetAddress1(o.getAddress1() + o.getAddress2());
-			}
-			location.setRegion(o.getState());
-			o.setLocation(location);
-			for (int i = 0; i < o.getProjects().size(); i++) {
-				FeedDateTimeDurationDTO dateTimeDuration = new FeedDateTimeDurationDTO();
-				List<FeedDateTimeDurationDTO> dateTimeDurations = new ArrayList<FeedDateTimeDurationDTO>();
-				dateTimeDurations.add(dateTimeDuration);
-				o.getProjects().get(i).setDateTimeDurations(dateTimeDurations);
-				List<FeedSponsoringOrganizationDTO> sponsoringOrganizations = new ArrayList<FeedSponsoringOrganizationDTO>();
-				FeedSponsoringOrganizationDTO sponsoringOrganization = new FeedSponsoringOrganizationDTO();
-				sponsoringOrganization.setOrganizationId(o.getProjects().get(i).getOrganizationId());
-				sponsoringOrganizations.add(sponsoringOrganization);
-				o.getProjects().get(i).setSponsoringIds(sponsoringOrganizations);
-				String flag = o.getProjects().get(i).getRemoteFlag();
-				if(flag.equals("Y")) {
-					o.getProjects().get(i).setVirtual("true");
-				} else {
-					o.getProjects().get(i).setVirtual("false");
-				}
-				projects.add(o.getProjects().get(i));
+    }
+    
+    private FeedLocationDTO populateFeedLocation(OrganizationDTO o) {
+    	FeedLocationDTO location = new FeedLocationDTO();
+		location.setCity(o.getCity());
+		location.setPostalCode(o.getZip());
+		if (o.getAddress1() == null || o.getAddress2() == null) {
+			if(o.getAddress1() == null && !((o.getAddress1() == null) && (o.getAddress2() == null))){
+				location.setStreetAddress1(o.getAddress2());
+			} else if (o.getAddress2() == null && !(o.getAddress1() == null && o.getAddress2() == null)) {
+				location.setStreetAddress1(o.getAddress1());
 			} 
-			afg.setProjects(projects);
+		} else {
+			location.setStreetAddress1(o.getAddress1() + o.getAddress2());
 		}
-        return afg;
+		location.setRegion(o.getState());
+		return location;
+    }
+	
+    private List<FeedDateTimeDurationDTO> populateDateTimeDurations() {
+    	FeedDateTimeDurationDTO dateTimeDuration = new FeedDateTimeDurationDTO();
+		List<FeedDateTimeDurationDTO> dateTimeDurations = new ArrayList<FeedDateTimeDurationDTO>();
+		dateTimeDurations.add(dateTimeDuration);
+		return dateTimeDurations;
+    }
+    
+    private FeedSponsoringOrganizationDTO populateSponsoringOrganization(OrganizationDTO o, int i) {
+		FeedSponsoringOrganizationDTO sponsoringOrganization = new FeedSponsoringOrganizationDTO();
+		sponsoringOrganization.setOrganizationId(o.getProjects().get(i).getOrganizationId());
+		return sponsoringOrganization;
+    }
+    
+    private void populateRemoteFlag(OrganizationDTO o, int i) {
+		String flag = o.getProjects().get(i).getRemoteFlag();
+		if(flag.equals("Y")) {
+			o.getProjects().get(i).setVirtual("true");
+		} else {
+			o.getProjects().get(i).setVirtual("false");
+		}
     }
 	
 }
