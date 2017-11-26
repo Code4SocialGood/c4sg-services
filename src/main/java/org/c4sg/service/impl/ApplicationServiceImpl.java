f1dee766e323a665c02d54559c32b0981d695c61
mathif92 <mathif925@gmail.com>
2017-11-26 02:34:52 -0300

    Issue #1474 finished (#401)
    * Issue #1474 finished
    
    * Issue #1473 finished

--- src/main/java/org/c4sg/service/impl/ApplicationServiceImpl.java
+++ src/main/java/org/c4sg/service/impl/ApplicationServiceImpl.java
@@ -197,7 +197,8 @@ public class ApplicationServiceImpl implements ApplicationService {
         	{
         		throw new UserProjectException("Already applied for the porject.");
         		
-        	} else if (status.equals("C") && java.util.Objects.nonNull(application.getAcceptedTime())){
+        	} else if (status.equals("C") && java.util.Objects.nonNull(application.getAcceptedTime()) && (java.util.Objects.isNull(application.getDeclinedTime()) ||
+                    (java.util.Objects.nonNull(application.getDeclinedTime()) && application.getAcceptedTime().after(application.getDeclinedTime())))){
         		
         		throw new UserProjectException("Already accepted for the project.");	
         		
@@ -241,6 +242,17 @@ public class ApplicationServiceImpl implements ApplicationService {
         		System.out.println("Application email sent: Project=" + project.getId() + " ; ApplicantEmail=" + user.getEmail() + " ; OrgEmail=" + orgUser.getEmail());
         	
         	} else if (application.getStatus().equals("C")) {
+				// send email to organization
+				Map<String, Object> contextOrg = new HashMap<String, Object>();
+				contextOrg.put("user", user);
+				contextOrg.put("skills", userSkills);
+				contextOrg.put("project", project);
+				contextOrg.put("projectLink", urlService.getProjectUrl(project.getId()));
+				contextOrg.put("userLink", urlService.getUserUrl(user.getId()));
+				contextOrg.put("application", application);
+				asyncEmailService.sendWithContext(Constants.C4SG_ADDRESS, orgUser.getEmail(),user.getEmail(), Constants.SUBJECT_APPLICAITON_ORGANIZATION, Constants.TEMPLATE_APPLICAITON_ACCEPT_ORGANIZATION, contextOrg);
+				System.out.println("Application email sent to org user: Project=" + project.getId() + " ; OrgUserEmail=" + orgUser.getEmail());
+
         		// send email to volunteer
        			Map<String, Object> contextVolunteer = new HashMap<String, Object>();
        			contextVolunteer.put("org", org);
@@ -248,7 +260,7 @@ public class ApplicationServiceImpl implements ApplicationService {
        			contextVolunteer.put("project", project);
        			contextVolunteer.put("projectLink", urlService.getProjectUrl(project.getId()));
        			asyncEmailService.sendWithContext(Constants.C4SG_ADDRESS, user.getEmail(), orgUser.getEmail(), Constants.SUBJECT_APPLICAITON_ACCEPT, Constants.TEMPLATE_APPLICAITON_ACCEPT, contextVolunteer);
-        		System.out.println("Application email sent: Project=" + project.getId() + " ; ApplicantEmail=" + user.getEmail());
+        		System.out.println("Application email sent to volunteer: Project=" + project.getId() + " ; ApplicantEmail=" + user.getEmail());
    
         	} else if (application.getStatus().equals("D")) {
         		// send email to volunteer
